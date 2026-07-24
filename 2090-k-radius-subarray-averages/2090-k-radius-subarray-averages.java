21efaa739a5e1e10e8d189422124f51f0ec68689
varshith devireddy <varshithdevireddy09@gmail.com>
2026-07-24 11:04:04 +0530

    Time: 6 ms (68.77%), Space: 175.3 MB (56.45%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int n = nums.length;
+        int[] arr = new int[n];
+        Arrays.fill(arr, -1);
+
+        int win = 2 * k + 1;
+        if (win > n) return arr;
+
+        long sum = 0;
+        int l = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+
+            if (i - l + 1 == win) {
+                arr[l + k] = (int)(sum / win);
+                sum -= nums[l];
+                l++;
+            }
+        }
+        return arr;
+    }
+}
