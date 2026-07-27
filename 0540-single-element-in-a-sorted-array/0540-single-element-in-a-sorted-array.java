9d9f20d590923e1d242105a1258507ddd54c179d
varshith devireddy <varshithdevireddy09@gmail.com>
2026-07-27 13:26:36 +0530

    Time: 24 ms (4.85%), Space: 52.7 MB (91.09%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,8 +1,40 @@
 class Solution {
+    public static int ub(int[] nums,int key){
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]<=key){
+                l = mid+1;
+            }
+            else {
+                r = mid-1;
+            }
+        }
+        return r;
+    }
+    public static int lb(int[] nums,int key){
+        int n = nums.length;
+        int l = 0;
+        int r = n-1;
+        while(l<=r){
+            int mid = l+(r-l)/2;
+            if(nums[mid]>=key){
+                r = mid-1;
+            }
+            else {
+                l= mid+1;
+            }
+        }
+        return l;
+    }
     public int singleNonDuplicate(int[] nums) {
-        int res = 0;
-        for(int num:nums) res^=num;
-
-        return res;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            int rep = ub(nums,nums[i])-lb(nums,nums[i])+1;
+            if(rep==1) return nums[i];
+        }
+       return 0; 
     }
 }
