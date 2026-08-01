0b0830a4cb3f08023df67fd1889c277f7983caec
varshith devireddy <varshithdevireddy09@gmail.com>
2026-08-01 11:53:15 +0530

    Time: 2 ms (49.49%), Space: 52.9 MB (45.82%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -9,21 +9,41 @@
  *     }
  * }
  */
+ 
 public class Solution {
+    public static ListNode collision(ListNode head1,ListNode head2,int d){
+        ListNode t2 = head2;
+        for(int i=0;i<d;i++){
+            t2 = t2.next;
+        }
+        ListNode t1 = head1;
+        while(t1!=t2){
+            t1 = t1.next;
+            t2 = t2.next;
+        }
+        return t1;
+
+    }
+    public static int count(ListNode head){
+        ListNode temp = head;
+        int count = 0;
+        while(temp!=null){
+            count = count+1;
+            temp = temp.next;
+        }
+        return count;
+    }
+
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashMap<ListNode,Integer> hm = new HashMap<>();
-        ListNode temp1 = headA;
-        while(temp1!=null){
-            hm.put(temp1,1);
-            temp1= temp1.next;
+        if(headA==null || headB==null) {
+            return null;
         }
-        ListNode temp2 = headB;
-        while(temp2!=null){
-           if(hm.containsKey(temp2)){
-             return temp2;
-           }
-           temp2 = temp2.next;
-        }
-        return null;
+
+        int n1 = count(headA);
+        int n2 = count(headB);
+        int d = Math.abs(n2-n1);
+        return (n1 < n2)
+        ? collision(headA, headB, d)
+        : collision(headB, headA, d);
     }
 }
