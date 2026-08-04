45616d636708656991417194d91ab059f8cea53b
varshith devireddy <varshithdevireddy09@gmail.com>
2026-08-04 09:52:10 +0530

    Time: 2 ms (71.5%), Space: 46.9 MB (94.82%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -5,14 +5,14 @@ class Solution {
         int rightmax = 1;
         int max = nums[0];
         for(int i=0;i<n;i++){
-        
+         leftmax = leftmax==0?1:leftmax;
+        rightmax = rightmax==0?1:rightmax;
         //front side product value
         leftmax*=nums[i];
         //from backside product value
         rightmax*=nums[n-1-i];
         max = Math.max(max,Math.max(leftmax,rightmax));
-        leftmax = leftmax==0?1:leftmax;
-        rightmax = rightmax==0?1:rightmax;
+       
         }
         return max;
     }
