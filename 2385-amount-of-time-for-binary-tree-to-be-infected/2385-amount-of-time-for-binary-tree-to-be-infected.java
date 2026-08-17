f25d6f438aa5278926333a8f3ae4dd22158451a6
varshith devireddy <varshithdevireddy09@gmail.com>
2026-08-17 15:17:26 +0530

    Time: 140 ms (9.18%), Space: 108.7 MB (87.43%) - LeetHub

---
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void markparent(HashMap<TreeNode,TreeNode> hm,TreeNode root,Queue<TreeNode> qp){
+        hm.put(root,null);
+        qp.offer(root);
+        while(!qp.isEmpty()){
+            TreeNode curr = qp.poll();
+            if(curr.left!=null){
+                hm.put(curr.left,curr);
+                qp.offer(curr.left);
+            }
+            if(curr.right!=null){
+                hm.put(curr.right,curr);
+                qp.offer(curr.right);
+            }
+        }
+        
+    }
+    public TreeNode check(TreeNode root,int start){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode curr = q.poll();
+            if(curr.val==start) return curr;
+            if(curr.left!=null) q.offer(curr.left);
+            if(curr.right!=null) q.offer(curr.right);
+        }
+        return null;
+    }
+    public int amountOfTime(TreeNode root, int start) {
+         if(root==null) return 0;
+          HashMap<TreeNode,TreeNode> hm = new HashMap<>();
+          Queue<TreeNode> qp = new LinkedList<>();
+          markparent(hm,root,qp);
+
+          HashSet<TreeNode> hs = new HashSet<>();
+          Queue<TreeNode> q = new LinkedList<>();
+          TreeNode target = check(root,start);
+          q.offer(target);
+          hs.add(target);
+          int level = 0;
+          while(!q.isEmpty()){
+             int len = q.size();
+             for(int i=0;i<len;i++){
+                TreeNode curr = q.poll();
+                if(curr.left!=null && !hs.contains(curr.left)){
+                    hs.add(curr.left);
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null && !hs.contains(curr.right)){
+                    hs.add(curr.right);
+                    q.offer(curr.right);
+                }
+                TreeNode parent = hm.get(curr);
+                if(parent!=null && !hs.contains(parent)){
+                    hs.add(parent);
+                    q.offer(parent);
+                }
+                
+             }
+             level++;
+          }
+          return level-1;
+    }
+}
