1633054204be8aeefbbd5ae0e905b9ccffbcf407
varshith devireddy <varshithdevireddy09@gmail.com>
2026-09-01 14:29:49 +0530

    Time: 10 ms (40.65%), Space: 52.6 MB (9.12%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,8 +1,10 @@
 class Solution {
-    public static int countpalin(String s,int left,int right){
+    public static int countpalin(String s,int left,int right,int[][] dp){
         int count = 0;
+        if(dp[left][right]!=-1) return dp[left][right];
         while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
             count++;
+            dp[left][right] = count;
             left--;
             right++;
         }
@@ -11,9 +13,13 @@ class Solution {
     public int countSubstrings(String s) {
         int n = s.length();
         int count = 0;
+        int[][] dp = new int[n+1][n+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
         for(int i=0;i<n;i++){
-            count+=countpalin(s,i,i);
-            count+=countpalin(s,i,i+1);
+            count+=countpalin(s,i,i,dp);
+            count+=countpalin(s,i,i+1,dp);
         }
         return count;
     }
